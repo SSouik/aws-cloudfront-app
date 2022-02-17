0d6f9d4d66d324d091ad3cf6f94af8dd1c81941c
Sam Souik <me@samuelsouik.com>
2022-02-16 21:30:15 -0600

    setup(initial commit): initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# aws-cloudfront-app
+
+This terraform module creates resources that can host an app using AWS Cloudfront.
---
+++ variables.tf
@@ -0,0 +1,5 @@
+variable "region" {
+  type = string
+  description = "Qualifying AWS region (Example: us-east-2)"
+  default = "us-east-1"
+}
